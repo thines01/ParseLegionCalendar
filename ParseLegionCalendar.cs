f533485f1cbd87234e8d7c005228c20d563d7398
thines01 <thines01@yahoo.com>
2022-08-04 07:51:53 -0500

    Updated

--- ParseLegionCalendar.cs
+++ ParseLegionCalendar.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/******************************************************************************\
+ * This program parses the online American Legion calendar and sends the result
+ * as an XLSX file in email to the recipients.
+ * 
+ * The output is used as part of the agenda for Post 0488 meetings.
+\******************************************************************************/
+using System;
 using System.IO;
 
 namespace ParseLegionCalendar
@@ -16,21 +22,25 @@ namespace ParseLegionCalendar
          DateTime dtNow = DateTime.Now;
          string strArgs = string.Format("SPECIAL={0}-{0}", dtNow.ToString("yyyyMMdd"));
          string strFileTitle = string.Format("Legion_Calendar_{0}_{1}.xlsx", dtNow.Year.ToString("D4"), dtNow.Month.ToString("D2"));
-         //
+
+         // Parameter below is unused in this feature
          string makeFileName(string s) => Path.Combine(Path.GetTempPath(), strFileTitle);
+
+         // delegate to deliver a specific (IDbMaster) Master to the process
          IDbMaster getMaster(DateTime dt1, DateTime dt2, string strReportFile) =>
             new CLegionCalObjMaster(strReportFile, dt1, 3);
+
          //
          CNowTime.TimeSay("Getting the Legion Calendar");
          
          if (!CMasterToAttachmentFileProcess.ProcessReport
             (
-               new string[] { strArgs },
-               getMaster,
+               new string[] { strArgs },  // can process multiple sets of args
+               getMaster,                 // master factory
 #if !DEBUG
-               "AMERICAN_LEGION",
+               "AMERICAN_LEGION",         // email group
 #else 
-               "TOM",
+               "TOM",                     // email group
 #endif
                ("American Legion Periodic Calendar (" + dtNow.ToString("y") + ")"),
                "This is an automated message\r\nAutomated by Tom Hines\r\n\r\nSee: attachment",
